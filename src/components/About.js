78685aef97892f15975bf57f88b76839fe03daf0
Your Name <you@example.com>
2023-05-21 17:40:46 +0530

    this is last

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const About = () => {
+  const link = 'https://github.com/harshitha-2201/moviesearchapp';
   return (
     <div>
-      <h1>About</h1>
+    <center>
+      <h1 className='text-blue-300 pt-16'>This is  About of React Movie App</h1>
+      <p className='text-white pt-3'>This is an React Movie Search App It Includes Movie Name , Poster,Geners,RealsedDate</p>
+      <p className='text-white'>If In case This Showing Nothing to related to movie Please Try Again with Correct Spell</p>
+      <p className='text-blue-100'>If you have Detail Information About this App Check Out Here</p>
+      <a href={link} className='text-blue-500 pt-3'>React Movie App</a>
+    </center>
     </div>
   )
 }
